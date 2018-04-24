2b45f4b2fcc5238fdfda8e8b9ea86967827eb72a
wangzhen <wangzhen@yunjiacloud.com>
2018-04-24 09:44:34 +0800

    banner图提交

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -3,12 +3,15 @@
     <!--search bar layout-->
     <div class="search-bar">
       <van-row gutter="5">
-        <van-col span="3"><img :src="locationIcon" width="80%" class="location-icon" /></van-col>
+        <van-col span="3">
+          <img :src="locationIcon" width="80%" class="location-icon" />
+        </van-col>
         <van-col span="16">
           <input type="text" class="search-input" placeholder="查找"/>
         </van-col>
-        <van-col span="5"><van-button size="mini">查找</van-button></van-col>
-
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
       </van-row>
     </div>
     <!--swipwer area-->
@@ -31,7 +34,7 @@
         bannerPicArray:[
           {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
           {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
-          {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'}
+          {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'},
         ]
       }
     },
