a648d642513dd53affa403a4d8b8febb17fb2ab3
wangzhen <wangzhen@yunjiacloud.com>
2018-04-29 22:30:08 +0800

    Mock数据的使用flex布局

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -16,11 +16,23 @@
     </div>
     <!--轮播图布局-->
     <div class="swiper-area">
-      <van-swipe :autoplay="1000">
+      <van-swipe :autoplay="3000">
         <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
-          <img v-lazy="banner.imageUrl" width="100%"/>
+          <img v-lazy="banner.image" width="100%"/>
         </van-swipe-item>
       </van-swipe>
+    </div>
+
+    <div class="type-bar">
+      <div  v-for="(cate,index) in category" :key="index" >
+        <img v-lazy="cate.image" width="90%" />
+        <span>{{cate.mallCategoryName}}</span>
+      </div>
+    </div>
+
+    <!--AD banner area-->
+    <div class="ad-banner">
+      <img v-lazy="adBanner.PICTURE_ADDRESS" width="100%">
     </div>
   </div>
 </template>
@@ -35,16 +47,22 @@
           {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
           {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
           {imageUrl:'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'},
-        ]
+        ],
+        category:[],
+        adBanner:[]
       }
     },
     created(){
-      console.log(111)
       axios({
         url: 'https://easy-mock.com/mock/5ae276d58e8556663763477a/vue-koa/',
         method: 'get',
       }).then(response => {
-        console.log(response)
+        console.log(response);
+        if(response.status==200){
+          this.category=response.data.data.category;
+          this.adBanner = response.data.data.advertesPicture //获得广告图片
+          this.bannerPicArray = response.data.data.slides   //轮播图片
+        }
       }).catch((error) => {
       })
     }
@@ -74,5 +92,19 @@
   .swiper-area{
     width:100%;
     overflow: hidden;
+  }
+  .type-bar{
+    background-color: #fff;
+    margin:0 .3rem .3rem .3rem;
+    border-radius: .3rem;
+    font-size:14px;
+    display: flex;
+    flex-direction:row;
+    flex-wrap:nowrap;
+  }
+  .type-bar div{
+    padding: .3rem;
+    font-size: 12px;
+    text-align: center;
   }
 </style>
