45c17009ed9fd687196c8af0bd455d046cd086aa
wangzhen <wangzhen@yunjiacloud.com>
2018-05-24 08:57:52 +0800

    mongoose

--- service/index.js
+++ service/index.js
@@ -1,10 +1,7 @@
-const Koa = require('koa')
-const app = new Koa()
+//引入connect
+const {connect} = require('./database/init.js')
 
-app.use(async(ctx)=>{
-  ctx.body = '<h1>hello Koa2</h1>'
-})
-
-app.listen(3000,()=>{
-  console.log('[Server] starting at port 3000')
-})
+//立即执行函数
+;(async () =>{
+  await connect()
+})()
